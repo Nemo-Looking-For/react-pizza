029cf7b5d0ac245d8751f9d88792ef22edffb5e6
Nemo-Looking-For <nnemshilov11@gmail.com>
2023-05-23 15:19:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/app.scss';
+
+
+import  Header from './components/header/Header';
+import  Categories from './components/categories/Categories';
+import  Sort from './components/sort/Sort';
+import PizzaBlock from './components/pizza-block/Pizza-block';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="wrapper">
+      <Header/>
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories/>
+            <Sort/>
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title='Чизбургер-пицца' price={420}/>
+            <PizzaBlock title='Сырная' price={399}/>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
